2b8f9a2f41815b19716e1e5589557b1fbd01261d
Atharv Aggarwal <aggarwal.atharv15@gmail.com>
2025-03-26 21:56:47 +0530

    Update fetch.py
    fixed typo

--- fetch.py
+++ fetch.py
@@ -3,6 +3,6 @@ import requests
 url=input("Enter the url")
 
 r=requests.get(url)
-with open("scrappedbse.html",'w+') as f:
+with open("scrapped.html",'w+') as f:
           f.write(r.text)
           print("done")
